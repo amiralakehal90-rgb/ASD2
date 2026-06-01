3701b0121a5adb0c50f82322459792a34cccb357
yasmine797 <yasmineblh77@gmail.com>
2026-06-01 17:09:54 +0100

    Update string_utils.h

--- include/string_utils.h
+++ include/string_utils.h
@@ -1,3 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+void sortDatasetByField(Record arr[], int count, char* field);
+
+#endif
+
 void toUpperCase(char* s);
 void toLowerCase(char* s);
 void reverseString(char* s);
