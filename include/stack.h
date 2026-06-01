db88f02062c9c47bee1741a69095d9db9351a1d7
yasmine797 <yasmineblh77@gmail.com>
2026-06-01 14:25:29 +0100

    Update stack.h

---
+++ include/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/* Borrow the Node definition from linked_list.h */
+#include "linked_list.h"
+
+/* Pointer-based Stack (linked list) */
+typedef struct Stack {
+    Node* top;    // top element
+} Stack;
+
+/* --- STACK FUNCTION PROTOTYPES --- */
+void push(Stack* S, int val);
+int pop(Stack* S);
+int peek(Stack* S);
+int isEmpty(Stack* S);
+void freeStack(Stack* S);
+
+#endif /* STACK_H */
