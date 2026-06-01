815bc715bad70ff8476e982a1f5e67a1808dd276
dhikraben007-ops <dhikraben007@gmail.com>
2026-06-01 21:00:36 +0100

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -1,67 +1,74 @@
+#include "../include/queue.h"      // It represent the path towards my header (library)
 #include <stdio.h>
 #include <stdlib.h>
 
-// This looks backward one directory, enters the include folder, and grabs queue.h
-#include "../include/queue.h" 
-
-int enqueue(Queue* Q, int value) {
-    // 1. Allocate memory for a new node
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) {
-        return 0; // Allocation failed
-    }
-
-    newNode->data = value;
-    newNode->next = NULL;
-
-    // 2. If the queue is empty, the new node is both front and rear
-    if (Q->rear == NULL) {
-        Q->front = Q->rear = newNode;
-    } else {
-        // 3. Link the new node to the end and update rear
-        Q->rear->next = newNode;
-        Q->rear = newNode;
-    }
-
-    // Return 1 for success
-    return 1; 
-}
-
-void initQueue(Queue* Q) {
+// fun 01
+ 
+void initQueue(Queue* Q){
     Q->front = NULL;
     Q->rear = NULL;
 }
 
-// إضافة عنصر للنهاية (Enqueue)
-int enqueue(Queue* Q, int value) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) return -1;
-    
-    newNode->data = value;
-    newNode->next = NULL;
+// fun 02
+ 
+int enqueue(Queue* Q, int value){
 
-    if (Q->rear == NULL) { // إذا كان الطابور فارغاً
-        Q->front = Q->rear = newNode;
-    } else {
-        Q->rear->next = newNode;
-        Q->rear = newNode;
+    /* Allocate memory for the new node */
+    Node* node = malloc(sizeof(Node));
+    if (node == NULL)
+        return -1;  /* Allocation failed */
+
+    node->data = value; 
+    node->next = NULL;   
+
+    /* If queue is empty, both front and rear point to the new node */
+    if (Q->front == NULL)
+    {
+        Q->front = node;
+        Q->rear  = node;
     }
+    else
+    {
+        Q->rear->next = node;  /* Link new node after current rear */
+        Q->rear = node;  /* Update rear to the new node */
+    }
+
     return 1;
 }
 
-// حذف عنصر من البداية (Dequeue)
-int dequeue(Queue* Q) {
-    if (Q->front == NULL) return -1;
+// fun 03
 
-    int value = Q->front->data;
-    Node* temp = Q->front;
-    
-    Q->front = Q->front->next;
-    
-    if (Q->front == NULL) {
+int dequeue(Queue* Q){
+   int value;
+    Node* temp;
+
+    if (Q->front == NULL)
+        return -1;  
+
+    value = Q->front->data;  /* Save the front value to return */
+    temp  = Q->front;        /* Save pointer to free it later */
+
+    Q->front = Q->front->next;  /* Move front to the next node */
+
+    if (Q->front == NULL)
         Q->rear = NULL;
-    }
-    
-    free(temp);
+
+    free(temp);  /* Free the old front node */
+
     return value;
 }
+
+// fun 04 
+
+#define MAX 5
+int front(Queue* Q)
+{
+    if (Q->size == 0)
+    {
+        printf("Queue is empty\n");
+        return -1;
+    }
+
+    return Q->data[Q->front];
+
+    return Q->data[(Q->rear - 1 + MAX) % MAX];
