c5173dec35ce717424335db173fbdb4892bd8538
yasmine797 <yasmineblh77@gmail.com>
2026-06-01 17:25:15 +0100

    Update string_utils.h

--- include/string_utils.h
+++ include/string_utils.h
@@ -3,8 +3,14 @@
 
 void sortDatasetByField(Record arr[], int count, char* field);
 
-#endif
+
 
 void toUpperCase(char* s);
 void toLowerCase(char* s);
 void reverseString(char* s);
+
+int my_strlen(const char* s);
+char* my_strcpy(char* dest, const char* src);
+char* my_strncpy(char* dest, const char* src, int n);
+
+#endif
