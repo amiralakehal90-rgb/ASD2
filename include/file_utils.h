26567eeb9317117b5165e5cab280757ff51d6d82
yasmine797 <yasmineblh77@gmail.com>
2026-06-01 15:03:17 +0100

    Update file_utils.h

--- include/file_utils.h
+++ include/file_utils.h
@@ -1,6 +1,40 @@
+#ifndef COMMON_H
+#define COMMON_H
+
 typedef struct {
     int   id;
     char  name[50];
     float score;
     char  category[20];
 } Record;
+
+/* Node for singly linked list */
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct List {
+    Node* head;
+    int size;
+} List;
+
+/* Node for doubly linked list */
+typedef struct DNode {
+    int data;
+    struct DNode* next;
+    struct DNode* prev;
+} DNode;
+
+// --- 2. تعريف هيكل الـ Bonus (الذي كان ينقصك) ---
+
+typedef struct {
+    int id;
+    char name[50];
+    char category[20]; // تأكدي من إضافة هذا السطر بالتحديد
+} Record;
+
+// --- 3. تصريحات الدوال (Prototypes) ---
+int countRecords(const char* filename);
+int appendRecord(const char* filename, Record* r);
+#endif
